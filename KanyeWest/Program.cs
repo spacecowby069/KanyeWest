60a13a47de834f67c211f4483ad2530ce11b8eec
Michael Farris <spacecowby069@yahoo.com>
2021-03-05 16:34:51 -0500

    My final commit

---
+++ KanyeWest/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeWest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kanyeURL = "https://api.kanye.rest";
+            
+            var client = new HttpClient();
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine(kanyeQuote);
+        }
+    }
+}
